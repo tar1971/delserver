a45cd8820f777d3f454151716f8e4a592e3bf694
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-15 03:25:32 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,26 +5,37 @@
 ###########################################
 ###########################################
 #!/bin/sh
+echo
 rm -rf /etc/enigma2/jediplaylists/playlists.txt
 wait
+echo
 rm -rf /etc/tuxbox/config/CCcam.cfg
 wait
+echo
 rm -rf /etc/tuxbox/config/ncam.server
 wait
+echo
 rm -rf /etc/tuxbox/config/oscam.server
 wait
+echo
 rm -rf /etc/enigma2/xstreamity/playlists.txt
 wait
+echo
 rm -rf /etc/enigma2/iptosat.conf
 wait
+echo
 rm -rf /etc/enigma2/ipaudio.json
 wait
+echo
 rm -rf /etc/enigma2/xstreamity/playlists.txt
 wait
+echo
 rm -rf /etc/enigma2/playlists
 wait
+echo
 rm -rf /etc/enigma2/playlist.e2pls
 wait
+echo
 rm -rf /etc/tsiplayer_xtream.conf
 wait
 sleep 2;
