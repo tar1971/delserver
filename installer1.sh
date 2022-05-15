8cb90604c8e774d6dc69f4a617213216d456f425
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-15 03:47:29 +0200

    Add files via upload

---
+++ installer1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+######################################################################################
+## Command=wget https://raw.githubusercontent.com/tar1971/delserver/main/installer1.sh -O - | /bin/sh
+##
+###########################################
+###########################################
+#!/bin/sh
+echo
+# Clean up
+
+#!/bin/sh
+echo
+rm -rf /etc/enigma2/xstreamity/playlists.txt
+wait
+sleep 2;
+exit
