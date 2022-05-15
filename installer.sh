93d1a321f4462cd883273103871588c8d9e42ba8
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-15 03:40:22 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -3,43 +3,32 @@
 ## Command=wget https://raw.githubusercontent.com/tar1971/delserver/main/installer.sh -O - | /bin/sh
 ##
 ###########################################
- Check Version
-####################################
-
+###########################################
 #!/bin/sh
-wait
-echo ''
-rm -rf /etc/enigma2/jediplaylists/playlists.txt
-wait
-echo ''
-rm -rf /etc/tuxbox/config/CCcam.cfg
-wait
 echo
-rm -rf /etc/tuxbox/config/ncam.server
+# Clean up
+
+rm -rf '/etc/enigma2/jediplaylists/playlists.txt'> /dev/null 2>&1
 wait
-echo ''
-rm -rf /etc/tuxbox/config/oscam.server
+rm -rf '/etc/tuxbox/config/CCcam.cfg'> /dev/null 2>&1
 wait
-echo ''
-rm -rf /etc/enigma2/xstreamity/playlists.txt
+rm -rf '/etc/tuxbox/config/ncam.server'> /dev/null 2>&1
 wait
-echo
-rm -rf /etc/enigma2/iptosat.conf
+rm -rf '/etc/tuxbox/config/oscam.server'> /dev/null 2>&1
 wait
-echo
-rm -rf /etc/enigma2/ipaudio.json
+rm -rf '/etc/enigma2/xstreamity/playlists.txt'> /dev/null 2>&1
 wait
-echo
-rm -rf /etc/enigma2/xstreamity/playlists.txt
+rm -rf '/etc/enigma2/iptosat.conf'> /dev/null 2>&1
 wait
-echo ''
-rm -rf /etc/enigma2/playlists
+rm -rf '/etc/enigma2/ipaudio.json'> /dev/null 2>&1
 wait
-echo ''
-rm -rf /etc/enigma2/playlist.e2pls
+rm -rf '/etc/enigma2/xstreamity/playlists.txtwait'>/dev/null 2>&1
 wait
-echo ''
-rm -rf /etc/tsiplayer_xtream.conf
+rm -rf '/etc/enigma2/playlists'> /dev/null 2>&1
+wait
+rm -rf '/etc/enigma2/playlist.e2pls'> /dev/null 2>&1
+wait
+rm -rf '/etc/tsiplayer_xtream.conf'> /dev/null 2>&1
 wait
 sleep 2;
 exit
@@ -52,3 +41,10 @@ rm -rf /etc/enigma2/xstreamity/playlists.txt
 wait
 sleep 2;
 exit
+-------------------------------------------------------------------------------------------------------------------------------
+
+rm -rf '/usr/lib/enigma2/python/Plugins/Extensions/AJPan'  > /dev/null 2>&1
+			rm -rf './usr/lib/enigma2/python/Plugins/Extensions/AJPan' > /dev/null 2>&1
+			rm -rf '/usr/lib/enigma2/python/Plugins/Extensions/AJPanel'  > /dev/null 2>&1
+			rm -rf './usr/lib/enigma2/python/Plugins/Extensions/AJPanel' > /dev/null 2>&1
+
